b5b3a3dc7cdba2a05ba57c06e4c5b2a9379bf92c
Moshe Cohen <moshec40@gmail.com>
2017-05-05 14:58:26 +0300

    * my test code removed

--- src/main/java/problem/predator/FeatureExtractor.java
+++ src/main/java/problem/predator/FeatureExtractor.java
@@ -9,17 +9,6 @@ public class FeatureExtractor {
 
         // *** YOUR CODE HERE **********************************************************************
 
-        state = new double[4];
-        for (int i = 0; i < predators.length; i++) {
-            if ((predators[i].x != caller.x) && (predators[i].y != caller.y)) {
-                state[0] = (int)((caller.x - predators[i].x) / 5.0);
-                state[1] = (int)((caller.y - predators[i].y) / 5.0);
-            }
-        }
-
-        state[2] = (int)((caller.x - preys[0].x) / 5.0);
-        state[3] = (int)((caller.y - preys[0].y) / 5.0);
-
         // *** END OF YOUR CODE ********************************************************************
 
         return state;
