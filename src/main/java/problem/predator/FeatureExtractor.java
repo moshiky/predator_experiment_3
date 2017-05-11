3af9e21e84922f090f32a3446b83e96b3fb86595
debug <debug>
2017-05-11 14:32:46 +0300

    code completed, 45 mins

--- src/main/java/problem/predator/FeatureExtractor.java
+++ src/main/java/problem/predator/FeatureExtractor.java
@@ -13,11 +13,37 @@ public class FeatureExtractor {
      * @return a representation of current state, as seen by thisPredator
      */
     public static double[] getStateRepresentation(Animal thisPredator, Animal otherPredator, Animal prey) {
-        double[] state = null;
+        double[] state = new double[6];
 
-        // *** YOUR CODE HERE **********************************************************************
+        /* Just give the locations
+        state[0] = otherPredator.x;
+        state[1] = otherPredator.y;
+        state[2] = prey.x;
+        state[3] = prey.y;
+        state[4] = thisPredator.x;*/
 
-        // *** END OF YOUR CODE ********************************************************************
+
+        state[0] = thisPredator.x > prey.x ? 1 : 0;
+        state[1] = thisPredator.y > prey.y ? 1 : 0;
+        state[2] = otherPredator.x > prey.x ? 1 : 0;
+        state[3] = otherPredator.y > prey.y ? 1 : 0;
+        state[4] = otherPredator.x > thisPredator.x ? 1 : 0;
+        state[5] = otherPredator.y > thisPredator.y ? 1 : 0;
+
+/*
+        double x0 = thisPredator.x > prey.x ? 1 : 0;
+        double y0 = thisPredator.y > prey.y ? 1 : 0;
+        double x1 = otherPredator.x > prey.x ? 1 : 0;
+        double y1 = otherPredator.y > prey.y ? 1 : 0;
+        double x2 = otherPredator.x > thisPredator.x ? 1 : 0;
+        double y2 = otherPredator.y > thisPredator.y ? 1 : 0;
+
+        state[0] = x0 + x1;
+        state[1] = y0 + y1;
+        state[2] = x2;
+        state[3] = y2;
+*/
+
 
         return state;
     }
