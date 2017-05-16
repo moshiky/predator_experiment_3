6423f49e473081452cec77b7d094bf4b2ca86b3e
moshiky <moshec40@gmail.com>
2017-05-16 17:28:47 +0300

    code completed

--- src/main/java/problem/predator/FeatureExtractor.java
+++ src/main/java/problem/predator/FeatureExtractor.java
@@ -17,9 +17,45 @@ public class FeatureExtractor {
 
         // *** YOUR CODE HERE **********************************************************************
 
+        state = new double[16];
+        state[0] = align( thisPredator.x - otherPredator.x );
+        state[1] = align( thisPredator.y - otherPredator.y);
+        state[2] = align( thisPredator.x - prey.x);
+        state[3] = align( thisPredator.y - prey.y);
+        state[4] = align(thisPredator.x - otherPredator.x - (thisPredator.y - otherPredator.y)) ;
+        state[5] = align( thisPredator.x - prey.x - (thisPredator.y - prey.y)) ;
+        state[6] = align ( otherPredator.x - prey.x );
+        state[7] = align( otherPredator.y - prey.y );
+        state[8] = align(thisPredator.x - prey.x - (otherPredator.x - prey.x) );
+        state[9] = align(thisPredator.y - prey.y - (otherPredator.y - prey.y) );
+        state[10] = align( Math.abs(thisPredator.x - prey.x)  - 2);
+        state[11] = align( Math.abs(thisPredator.y - prey.y)  - 2);
+        state[12]= prey.x == 0 ? 0 : 1;
+        state[13]= prey.y == 0 ? 0 : 1;
+        state[14]= prey.x == 19 ? 0 : 1;
+        state[15]= prey.y == 19 ? 0 : 1;
+//        state[14]= otherPredator.x == 0 ? 0 : 1;
+//        state[15]= otherPredator.y == 0 ? 0 : 1;
+//        state[16]= otherPredator.x == 19 ? 0 : 1;
+//        state[17]= otherPredator.y == 19 ? 0 : 1;
+//        state[18]= thisPredator.x == 0 ? 0 : 1;
+//        state[19]= thisPredator.y == 0 ? 0 : 1;
+//        state[20]= thisPredator.x == 19 ? 0 : 1;
+//        state[21]= thisPredator.y == 19 ? 0 : 1;
+
+
+
         // *** END OF YOUR CODE ********************************************************************
 
         return state;
+    }
+
+    private static double align(double a)
+    {
+        if (a < 0)
+            return -1;
+        else
+            return a > 0 ? 1 : 0;
     }
 
     /**
