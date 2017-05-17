0ac67a53afca0b4589f095e7b8357e2a1adf3b03
Oleg <oleg@maksimov.co.il>
2017-05-17 07:07:25 -0700

    code completed

--- src/main/java/problem/predator/SimilarityManager.java
+++ src/main/java/problem/predator/SimilarityManager.java
@@ -23,12 +23,107 @@ public class SimilarityManager {
      * @return
      */
     public static ArrayList<SimilarityRecord> getSimilarityRecords(double[] state, int action) {
-        ArrayList<SimilarityRecord> similarityRecords = new ArrayList<>();
-
+            ArrayList<SimilarityRecord> similarityRecords = new ArrayList<>();
         // *** YOUR CODE HERE **********************************************************************
-
+        // find end state
+        double[] end_state = new double[4];
+        switch(action){
+            case 0:
+                //up
+                if(state[1]-1<=-20 || state[3]-1<=-20){
+                    end_state = state;
+                }
+                else {
+                    end_state[0] = state[0];
+                    end_state[1] = state[1] - 1;
+                    end_state[2] = state[2];
+                    end_state[3] = state[3] - 1;
+                }
+                break;
+            case 1:
+                //down
+                if(state[1]+1>=20 || state[3]+1>=20){
+                    end_state = state;
+                }
+                else {
+                    end_state[0] = state[0];
+                    end_state[1] = state[1] + 1;
+                    end_state[2] = state[2];
+                    end_state[3] = state[3] + 1;
+                }
+                break;
+            case 2:
+                //left
+                if(state[0]-1<=-20 || state[2]-1<=-20){
+                    end_state = state;
+                }
+                else {
+                    end_state[0] = state[0] - 1;
+                    end_state[1] = state[1];
+                    end_state[2] = state[2] - 1;
+                    end_state[3] = state[3];
+                }
+                break;
+            case 3:
+                //right
+                if(state[0]+1>=20 || state[2]+1>=20){
+                    end_state = state;
+                }
+                else {
+                    end_state[0] = state[0] + 1;
+                    end_state[1] = state[1];
+                    end_state[2] = state[2] + 1;
+                    end_state[3] = state[3];
+                }
+                break;
+            case 4:
+                //stay
+                end_state = state;
+                break;
+        }
+        // find all states that go to end state - those are the most similar states to the current state!
+        //up
+        double[] current_state = new double[4];
+        SimilarityRecord sm;
+        if(state[1]-1>-20 || state[3]-1>-20) {
+            current_state[0] = end_state[0];
+            current_state[1] = end_state[1] - 1;
+            current_state[2] = end_state[2];
+            current_state[3] = end_state[3] - 1;
+            sm = new SimilarityRecord(current_state, 0, 1);
+            similarityRecords.add(sm);
+        }
+        //down
+        if(state[1]+1<20 || state[3]+1<20) {
+            current_state[0] = end_state[0];
+            current_state[1] = end_state[1] + 1;
+            current_state[2] = end_state[2];
+            current_state[3] = end_state[3] + 1;
+            sm = new SimilarityRecord(current_state, 1, 1);
+            similarityRecords.add(sm);
+        }
+        //left
+        if(state[0]-1>-20 || state[2]-1>-20) {
+            current_state[0] = end_state[0] - 1;
+            current_state[1] = end_state[1];
+            current_state[2] = end_state[2] - 1;
+            current_state[3] = end_state[3];
+            sm = new SimilarityRecord(current_state, 2, 1);
+            similarityRecords.add(sm);
+        }
+        //right
+        if(state[0]+1<20 || state[2]+1<20) {
+            current_state[0] = end_state[0] + 1;
+            current_state[1] = end_state[1];
+            current_state[2] = end_state[2] + 1;
+            current_state[3] = end_state[3];
+            sm = new SimilarityRecord(current_state, 3, 1);
+            similarityRecords.add(sm);
+        }
+        //stay
+        sm = new SimilarityRecord(end_state, 4, 1);
+        similarityRecords.add(sm);
         // *** END OF YOUR CODE ********************************************************************
-
         return similarityRecords;
     }
 }
