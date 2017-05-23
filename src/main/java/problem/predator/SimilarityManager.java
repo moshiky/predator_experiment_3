59160dc7164a268fefcb88788c88aa6a35b0b267
moshiky <moshec40@gmail.com>
2017-05-23 13:56:17 +0300

    * code completed

--- src/main/java/problem/predator/SimilarityManager.java
+++ src/main/java/problem/predator/SimilarityManager.java
@@ -25,9 +25,65 @@ public class SimilarityManager {
     public static ArrayList<SimilarityRecord> getSimilarityRecords(double[] state, int action) {
         ArrayList<SimilarityRecord> similarityRecords = new ArrayList<>();
 
-        // *** YOUR CODE HERE **********************************************************************
+        for (int i = 0; i < 5; i++) {
+            if (i != action){
+                if (i ==4) {
+                    similarityRecords.add(new SimilarityRecord(state.clone(),i, 0.75));
+                } else {
+                    similarityRecords.add(new SimilarityRecord(state.clone(),i, 0.5));
+                }
+            }
+        }
 
-        // *** END OF YOUR CODE ********************************************************************
+
+        /*
+        double[] similarState = new double[4];
+        double factor;
+        similarState[0] = state[0];
+        similarState[1] = state[1];
+        switch (action) {
+            case 0:
+                similarState[3] = state[3];
+                for (int i = -19; i < 20; i++) {
+                    if (i == state[2]) {
+                        break;
+                    }
+                    similarState[2] = i;
+                    factor = i > state[2] ? 0 : (state[2] - i)/(state[2] + 19);
+                    similarityRecords.add(new SimilarityRecord(similarState.clone(), action, factor));
+                }
+                // Adding cases for other.
+        }
+
+ /*       double[] similarState = new double[4];
+        SimilarityRecord record;
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                for (int l = 0; l < 20; l++) {
+                    for (int k = 0; k< 20; k++) {
+                        similarState[0] = i;
+                        similarState[1] = j;
+                        similarState[2] = l;
+                        similarState[3] = k;
+                        record = SimilarityRecord(similarState, action, )
+                        similarityRecords.add()
+                    }
+                }
+            }
+        }
+        double[] similarState = new double[4];
+        double factor;
+        similarState[0] = state[0];
+        similarState[1] = state[1];
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                similarState[2] = i;
+                similarState[3] = j;
+                factor = (i - state[2] + j - state[3])/4;
+                similarityRecords.add(new SimilarityRecord(similarState.clone(), action, factor));
+
+            }
+        }*/
 
         return similarityRecords;
     }
