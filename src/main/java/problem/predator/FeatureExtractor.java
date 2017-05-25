e95ddf7066f6344e809d1dc2689d1936d6137e0e
moshiky <moshec40@gmail.com>
2017-05-25 16:09:44 +0300

    * code completed

--- src/main/java/problem/predator/FeatureExtractor.java
+++ src/main/java/problem/predator/FeatureExtractor.java
@@ -1,5 +1,10 @@
 package problem.predator;
 
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Dev on 26/04/2017.
  */
@@ -16,7 +21,61 @@ public class FeatureExtractor {
         double[] state = null;
 
         // *** YOUR CODE HERE **********************************************************************
+        List<Double> list = new LinkedList<>();
+//        list.add((double) (thisPredator.x - otherPredator.x));
+//        list.add((double) (thisPredator.y - otherPredator.y));
+        int dist_bin = (thisPredator.x + otherPredator.x - prey.x) > 0 &&
+                (thisPredator.x + otherPredator.x - prey.x) < 3 &&
+                (thisPredator.y + otherPredator.y - prey.y) > 0 &&
+                (thisPredator.y + otherPredator.y - prey.y) < 3 ? 1: 0;
+        list.add((double) dist_bin);
+        dist_bin = (thisPredator.x + otherPredator.x - prey.x) > 5 &&
+                (thisPredator.y + otherPredator.y - prey.y) > 5 ? 1: 0;
+        list.add((double) dist_bin);
+        dist_bin = (thisPredator.x + otherPredator.x - prey.x) > 10 &&
+                (thisPredator.y + otherPredator.y - prey.y) > 10 ? 1: 0;
+        list.add((double) dist_bin);
+//        list.add((double) (thisPredator.x - prey.x));
+//        list.add((double) (thisPredator.y - prey.y));
+//        list.add((double) (otherPredator.x - prey.x));
+//        list.add((double) (otherPredator.y - prey.y));
 
+        int pin = (prey.x == 19 && prey.y == 19) ? 1: 0;
+        list.add(new Double(pin));
+        pin = (prey.x == 0 && prey.y == 0) ? 1: 0;
+        list.add(new Double(pin));
+        pin = (prey.x == 0 && prey.y == 19) ? 1: 0;
+        list.add(new Double(pin));
+        pin = (prey.x == 19 && prey.y == 0) ? 1: 0;
+        list.add(new Double(pin));
+
+        /*pin = prey.x == 0 ? 1: 0;
+        list.add(new Double(pin));
+        pin = prey.x == 19 ? 1: 0;
+        list.add(new Double(pin));
+        pin = prey.y == 0 ? 1: 0;
+        list.add(new Double(pin));
+        pin = prey.y == 19 ? 1: 0;
+        list.add(new Double(pin));*/
+
+        pin = (prey.x == thisPredator.x && prey.x == otherPredator.x && thisPredator.y < otherPredator.y) ? 1: 0;
+        list.add(new Double(pin));
+        pin = (prey.x == thisPredator.x && prey.x == otherPredator.x && thisPredator.y > otherPredator.y) ? 1: 0;
+        list.add(new Double(pin));
+        pin = (prey.y == thisPredator.y && prey.y == otherPredator.y && thisPredator.x > otherPredator.x) ? 1: 0;
+        list.add(new Double(pin));
+        pin = (prey.y == thisPredator.y && prey.y == otherPredator.y && thisPredator.x < otherPredator.x) ? 1: 0;
+        list.add(new Double(pin));
+
+        /*pin = (thisPredator.y == otherPredator.y) ? 1: 0;
+        list.add(new Double(pin));
+        pin = (thisPredator.x == otherPredator.x) ? 1: 0;
+        list.add(new Double(pin));*/
+
+
+//        state = ArrayUtils.toPrimitive((Double[])list.toArray());
+        Double[] temp = list.toArray(new Double[list.size()]);
+        state = ArrayUtils.toPrimitive(temp);
         // *** END OF YOUR CODE ********************************************************************
 
         return state;
