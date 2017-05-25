db1df5d63f34f89c26fea2d811bc22081077903e
moshiky <moshec40@gmail.com>
2017-05-25 15:36:40 +0300

    * code completed

--- src/main/java/problem/predator/SimilarityManager.java
+++ src/main/java/problem/predator/SimilarityManager.java
@@ -22,10 +22,76 @@ public class SimilarityManager {
      *               4: STAY
      * @return
      */
-    public static ArrayList<SimilarityRecord> getSimilarityRecords(double[] state, int action) {
+    public static ArrayList<SimilarityRecord> getSimilarityRecords(double[] state, int action)
+    {
         ArrayList<SimilarityRecord> similarityRecords = new ArrayList<>();
 
         // *** YOUR CODE HERE **********************************************************************
+
+        if(action == 4)
+        {
+            similarityRecords.add(new SimilarityRecord(state, action, 1));
+        }
+
+        double []newState = new double[4];
+
+        if(action == 2) {
+            // for one step left:
+            for (int i = 0; i < 4; i++)
+                newState[i] = state[i];
+
+            if (newState[1] > 0)
+                newState[1] -= 1;
+
+            if (newState[3] > 0)
+                newState[3] -= 1;
+
+            similarityRecords.add(new SimilarityRecord(newState, 4, 0.95));
+        }
+
+        if(action == 0) {
+            // one step up
+            for (int i = 0; i < 4; i++)
+                newState[i] = state[i];
+
+            if (newState[1] > 0)
+                newState[1] -= 1;
+
+            if (newState[3] > 0)
+                newState[3] -= 1;
+
+            similarityRecords.add(new SimilarityRecord(newState, 4, 0.95));
+        }
+        if(action == 3) {
+            // onr step right
+            for (int i = 0; i < 4; i++)
+                newState[i] = state[i];
+
+            if (newState[0] < 19)
+                newState[0] += 1;
+
+            if (newState[2] < 19)
+                newState[2] += 1;
+
+            similarityRecords.add(new SimilarityRecord(newState, 4, 0.95));
+        }
+        // one step down:
+
+        if(action == 1) {
+            // onr step down
+            for (int i = 0; i < 4; i++)
+                newState[i] = state[i];
+
+            if (newState[1] < 19)
+                newState[1] += 1;
+
+            if (newState[3] < 19)
+                newState[3] += 1;
+
+
+            similarityRecords.add(new SimilarityRecord(newState, 4, 0.95));
+        }
+        //similarityRecords.add(new SimilarityRecord(state, action, 0.95));
 
         // *** END OF YOUR CODE ********************************************************************
 
