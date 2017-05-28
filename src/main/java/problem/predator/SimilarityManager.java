f5007bee023fcf29fec385dff56887635e1da6bb
moshiky <moshec40@gmail.com>
2017-05-28 15:01:29 +0300

    * code completed

--- src/main/java/problem/predator/SimilarityManager.java
+++ src/main/java/problem/predator/SimilarityManager.java
@@ -7,6 +7,65 @@ import java.util.ArrayList;
  */
 public class SimilarityManager {
 
+    public static double[] flipStateHorizontally(double[] state) {
+        double[] newState = new double[4];
+        newState[0] = -state[0];
+        newState[1] = state[1];
+        newState[2] = -state[2];
+        newState[3] = state[3];
+
+        return newState;
+    }
+
+    public static double[] flipStateVertically(double[] state) {
+        double[] newState = new double[4];
+        newState[0] = state[0];
+        newState[1] = -state[1];
+        newState[2] = state[2];
+        newState[3] = -state[3];
+
+        return newState;
+    }
+
+    public static double[] flipPrey(double[] state) {
+        double[] newState = new double[4];
+        newState[0] = state[0];
+        newState[1] = state[1];
+        newState[2] = -state[2];
+        newState[3] = -state[3];
+
+        return newState;
+    }
+
+    public static double[] flipPreyX(double[] state) {
+        double[] newState = new double[4];
+        newState[0] = state[0];
+        newState[1] = state[1];
+        newState[2] = -state[2];
+        newState[3] = state[3];
+
+        return newState;
+    }
+    public static double[] flipPreyY(double[] state) {
+        double[] newState = new double[4];
+        newState[0] = state[0];
+        newState[1] = state[1];
+        newState[2] = -state[2];
+        newState[3] = state[3];
+
+        return newState;
+    }
+
+    public static double[] move(double[] state, int movementX, int movementY) {
+        double[] newState = new double[4];
+        newState[0] = Math.min(-19., Math.max(state[0] + movementX, 19.));
+        newState[1] = Math.min(-19., Math.max(state[1] + movementY, 19.));
+        newState[2] = Math.min(-19., Math.max(state[2] + movementX, 19.));
+        newState[3] = Math.min(-19., Math.max(state[3] + movementY, 19.));
+
+        return newState;
+    }
+
     /**
      *
      * @param state represents the state:
@@ -26,6 +85,48 @@ public class SimilarityManager {
         ArrayList<SimilarityRecord> similarityRecords = new ArrayList<>();
 
         // *** YOUR CODE HERE **********************************************************************
+
+        switch (action) {
+            case 0:
+                similarityRecords.add(new SimilarityRecord(flipStateVertically(state), 1, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPrey(state), 1, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPreyY(state), 1, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPreyX(state), 0, 1.0));
+
+                similarityRecords.add(new SimilarityRecord(move(state,0,-1), 4, 1.0));
+                break;
+            case 1:
+                similarityRecords.add(new SimilarityRecord(flipStateVertically(state), 0, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPrey(state), 0, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPreyY(state), 0, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPreyX(state), 1, 1.0));
+                similarityRecords.add(new SimilarityRecord(move(state,0,1), 4, 1.0));
+                break;
+            case 2:
+                similarityRecords.add(new SimilarityRecord(flipStateHorizontally(state), 3, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPrey(state), 3, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPreyX(state), 3, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPreyY(state), 2, 1.0));
+                similarityRecords.add(new SimilarityRecord(move(state,-1,0), 4, 1.0));
+                break;
+            case 3:
+                similarityRecords.add(new SimilarityRecord(flipStateHorizontally(state), 2, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPrey(state), 2, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPreyX(state), 2, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipPreyY(state), 3, 1.0));
+                similarityRecords.add(new SimilarityRecord(move(state,1, 0), 4, 1.0));
+                break;
+            case 4:
+                similarityRecords.add(new SimilarityRecord(flipStateVertically(state), 4, 1.0));
+                similarityRecords.add(new SimilarityRecord(flipStateHorizontally(state), 4, 1.0));
+
+
+                similarityRecords.add(new SimilarityRecord(move(state,0,-1), 1, 1.0));
+                similarityRecords.add(new SimilarityRecord(move(state,0,1), 0, 1.0));
+                similarityRecords.add(new SimilarityRecord(move(state,1,0), 2, 1.0));
+                similarityRecords.add(new SimilarityRecord(move(state,-1,0), 3, 1.0));
+                break;
+        }
 
         // *** END OF YOUR CODE ********************************************************************
 
