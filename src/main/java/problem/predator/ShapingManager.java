e8a89d643e929afd8996c834b052fc6b96b53aec
moshiky <moshec40@gmail.com>
2017-06-04 15:54:25 +0300

    * code completed

--- src/main/java/problem/predator/ShapingManager.java
+++ src/main/java/problem/predator/ShapingManager.java
@@ -39,6 +39,22 @@ public class ShapingManager {
 
         // *** YOUR CODE HERE **********************************************************************
 
+
+        rewardShaping -= Math.abs(currentState[2]) - Math.abs(previousState[2]);
+        rewardShaping -= Math.abs(currentState[3]) - Math.abs(previousState[3]);
+
+//        System.out.println(Math.log10((currentState[0] - currentState[2])*(currentState[0] - currentState[2])
+//                + (currentState[1] - currentState[3])*(currentState[1] - currentState[3]) + 1.1));
+//        rewardShaping -= Math.sqrt((previousSt[0])*(currentState[0])
+//                + (currentState[1])*(currentState[1]));
+
+//        double dx = currentState[0] - previousState[0];
+//        double dy = currentState[1] - previousState[1];
+//        double dx2 = currentState[2] - previousState[2];
+//        double dy2 = currentState[3] - previousState[3];
+//        rewardShaping -= Math.sqrt((dx - dx2)*(dx - dx2)
+//                        + (dy - dy2)*(dy - dy2));
+
         // *** END OF YOUR CODE ********************************************************************
 
         return rewardShaping;
