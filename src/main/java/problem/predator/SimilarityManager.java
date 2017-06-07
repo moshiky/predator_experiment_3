c68ebd2109f690b4336efc7483c26714957e26ae
Moshe Cohen <moshec40@gmail.com>
2017-06-07 16:04:29 +0300

    * code completed

--- src/main/java/problem/predator/SimilarityManager.java
+++ src/main/java/problem/predator/SimilarityManager.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 /**
  * Created by Dev on 27/04/2017.
  */
-public class SimilarityManager {
+public class SimilarityManager
+{
 
     /**
      *
@@ -22,13 +23,62 @@ public class SimilarityManager {
      *               4: STAY
      * @return
      */
-    public static ArrayList<SimilarityRecord> getSimilarityRecords(double[] state, int action) {
+    public static ArrayList<SimilarityRecord> getSimilarityRecords(double[] state, int action)
+    {
         ArrayList<SimilarityRecord> similarityRecords = new ArrayList<>();
 
-        // *** YOUR CODE HERE **********************************************************************
+        double distp= Math.sqrt( Math.pow(state[2], 2) + Math.pow(state[3], 2));
+        double distf= Math.sqrt( Math.pow(state[0], 2) + Math.pow(state[1], 2));
+        double [] temp= new double[4];
+        SimilarityRecord sm= new SimilarityRecord(state, action, 1);
 
-        // *** END OF YOUR CODE ********************************************************************
+        for(int x=0; x<20; x++) {
 
+            for (int y = 0; y < 20; y++) {
+
+                for (int i = 0; i < 20; i++) {
+                    for (int j = 0; j < 20; j++) {
+                        double distOp = Math.sqrt(Math.pow(i, 2) + Math.pow(j, 2));
+                        double distOf = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+
+                        if (distp == distOp || distp == -distOp) {
+                            temp[2] = state[2];
+                            temp[3] = state[3];
+                        }
+                    }
+                }
+            }
+        }
+        /*for(int i=0; i<20; i++)
+        {
+            for(int j=0; j<20; j++)
+            {
+                //if(j+ )
+                if(((int)state[2] == i || (int)state[2] == -i) && ((int)state[3] == j || (int)state[3] == -j))
+                {
+                    //SimilarityRecord sm= new SimilarityRecord(state, action, 1);
+                    similarityRecords.add(new SimilarityRecord(state, action, 1));
+
+
+                }
+            }
+        }
+
+        for(int i=0; i<similarityRecords.size(); i++)
+        {
+            if(!((int)state[0] == similarityRecords.get(i).getState()[0] || (int)state[0] == -similarityRecords.get(i).getState()[0]))
+            {
+                similarityRecords.remove(i);
+            }
+        }
+
+        for(int i=0; i<similarityRecords.size(); i++)
+        {
+            if(!((int)state[1] == similarityRecords.get(i).getState()[1] || (int)state[1] == -similarityRecords.get(i).getState()[1]))
+            {
+                similarityRecords.remove(i);
+            }
+        }*/
         return similarityRecords;
     }
 }
