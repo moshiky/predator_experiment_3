a22ac31794e04126cf3e8c2a12c5e5a5142f5986
moshiky <moshec40@gmail.com>
2017-06-08 09:25:39 +0300

    * code completed

--- src/main/java/problem/predator/FeatureExtractor.java
+++ src/main/java/problem/predator/FeatureExtractor.java
@@ -17,6 +17,48 @@ public class FeatureExtractor {
 
         // *** YOUR CODE HERE **********************************************************************
 
+        state = new double[2];
+
+        // FIRST TRY:
+        // just put a abs on the distance
+        /*state[0] = Math.abs(thisPredator.x - otherPredator.x);
+        state[1] = Math.abs(thisPredator.y - otherPredator.y);
+        state[2] = Math.abs(thisPredator.x - prey.x);
+        state[3] = Math.abs(thisPredator.y - prey.y);*/
+
+        // second try: lets use distance formula!!
+        /*state[0] = Math.sqrt(Math.pow(thisPredator.x - otherPredator.x,2) + Math.pow(thisPredator.y - otherPredator.y,2));
+        state[1] = Math.sqrt(Math.pow(otherPredator.x - prey.x,2) + Math.pow(otherPredator.y - prey.y,2));
+        state[2] = Math.sqrt(Math.pow(thisPredator.x - prey.x,2) + Math.pow(thisPredator.y - prey.y,2));
+*/
+
+        // third try! (THIS ONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE)
+       /* state[0] = thisPredator.x - otherPredator.x;
+        state[1] = thisPredator.y - otherPredator.y;
+        state[2] = thisPredator.x - prey.x;
+        state[3] = thisPredator.y - prey.y;
+
+        state[4] = otherPredator.x - prey.x;
+        state[5] = otherPredator.y - prey.y;*/
+
+        // last try:
+        /*
+        state[0] = thisPredator.x - otherPredator.x;
+        state[1] = thisPredator.y - otherPredator.y;
+        state[2] = thisPredator.x - prey.x;
+        state[3] = thisPredator.y - prey.y;
+
+        state[4] = otherPredator.x - prey.x;
+        state[5] = otherPredator.y - prey.y;
+        state[6] = Math.sqrt(Math.pow(thisPredator.x - otherPredator.x,2) + Math.pow(thisPredator.y - otherPredator.y,2));
+        state[7] = Math.sqrt(Math.pow(otherPredator.x - prey.x,2) + Math.pow(otherPredator.y - prey.y,2));
+        state[8] = Math.sqrt(Math.pow(thisPredator.x - prey.x,2) + Math.pow(thisPredator.y - prey.y,2));
+*/
+
+        state[0] = thisPredator.x - prey.x;
+        state[1] = thisPredator.y - prey.y;
+
+
         // *** END OF YOUR CODE ********************************************************************
 
         return state;
