317fdb7ff1c144010f9739db2ffe060042886c13
moshiky <moshec40@gmail.com>
2017-06-18 13:16:25 +0300

    * code completed

--- src/main/java/problem/predator/FeatureExtractor.java
+++ src/main/java/problem/predator/FeatureExtractor.java
@@ -13,11 +13,38 @@ public class FeatureExtractor {
      * @return a representation of current state, as seen by thisPredator
      */
     public static double[] getStateRepresentation(Animal thisPredator, Animal otherPredator, Animal prey) {
-        double[] state = null;
 
         // *** YOUR CODE HERE **********************************************************************
+        //OPTION ONE
+       /* double[] state = new double[3];
+        state[0] = Math.pow(thisPredator.x - otherPredator.x,2) +Math.pow( thisPredator.y - otherPredator.y,2);
+       // state[1] = Math.sqrt( Math.pow( thisPredator.x - prey.x,2) +Math.pow( thisPredator.y - prey.y,2));
 
+        state[1] = thisPredator.x - prey.x;
+        state[2] = thisPredator.y - prey.y;*/
         // *** END OF YOUR CODE ********************************************************************
+        //CHECK 1 -- sum up the manhaten distances -- not good
+           /* double[] state = new double[2];
+            state[0] = Math.abs(thisPredator.x - prey.x)+ Math.abs(thisPredator.y - prey.y);
+            state[1] = Math.abs(thisPredator.x - otherPredator.x)+ Math.abs(thisPredator.y - otherPredator.y);*/
+
+
+        //CHECK 2 --dis x and y from pray and all in all distance from other predetor -- around 32
+       /* double[] state = new double[3];
+        state[0] = thisPredator.x - prey.x;
+        state[1] = thisPredator.y - prey.y;
+        state[2] =  Math.abs(thisPredator.x - otherPredator.x)+ Math.abs(thisPredator.y - otherPredator.y);*/
+
+       //CHECK 3  --> 25 steps
+       /* double[] state = new double[2];
+        state[0] = thisPredator.x - prey.x;
+        state[1] = thisPredator.y - prey.y;*/
+       // state[2] =  Math.abs(thisPredator.x - otherPredator.x)+ Math.abs(thisPredator.y - otherPredator.y);
+
+        double[] state = new double[2];
+        state[0] = thisPredator.x - prey.x;
+        state[1] = thisPredator.y - prey.y;
+       // state[2] = Math.sqrt( Math.abs(thisPredator.x - otherPredator.x)+ Math.abs(thisPredator.y - otherPredator.y));
 
         return state;
     }
