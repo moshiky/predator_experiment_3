b7a0283e4ad9a31cc90fb5446fcf063e7f915424
moshiky <moshec40@gmail.com>
2017-06-21 11:17:52 +0300

    * code completed

--- src/main/java/problem/predator/SimilarityManager.java
+++ src/main/java/problem/predator/SimilarityManager.java
@@ -26,7 +26,34 @@ public class SimilarityManager {
         ArrayList<SimilarityRecord> similarityRecords = new ArrayList<>();
 
         // *** YOUR CODE HERE **********************************************************************
+        //double[] newState = state.clone();
+        //int count1 =0 ,count2 =0 ;
+        for (int i = -19 ; i < 20 ; i++) {
+            double[] newState = state.clone();
+            newState[0] = newState[1]=newState[2]=newState[3]=-21;
+            for (int j = -19; j < 20; j++) {
+               if( i*i + j*j == state[0] * state[0] + state[1] * state[1]) {
+                   newState[0] = i;
+                   newState[1] = j;
+               }
+                if( i*i + j*j == state[2] * state[2] + state[3] * state[3]) {
+                    newState[2] = i;
+                    newState[3] = j;
+                }
+                if(newState[0] != -21 && newState[1] != -21 && newState[2] != -21 && newState[3] != -21 )
+                {
+                    for (int k = 0; k < 5; k++) {
+                        similarityRecords.add(new SimilarityRecord(newState, k, 0.9));
+                    }
+                }
+            }
+        }
+        //    similarityRecords.add(new SimilarityRecord(newState,0 , 0.5));
+      //  similarityRecords.add(new SimilarityRecord(newState, 1, 0.5));
+     //   similarityRecords.add(new SimilarityRecord(newState, , 0.5));
+      //  similarityRecords.add(new SimilarityRecord(newState, , 0.5));
 
+        //}
         // *** END OF YOUR CODE ********************************************************************
 
         return similarityRecords;
