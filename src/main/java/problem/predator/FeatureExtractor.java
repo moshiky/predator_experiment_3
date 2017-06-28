b48d6b842e4f8ac45a0cd021ff0a2a95871fab95
moshiky <moshec40@gmail.com>
2017-06-28 10:36:26 +0300

    * code completed

--- src/main/java/problem/predator/FeatureExtractor.java
+++ src/main/java/problem/predator/FeatureExtractor.java
@@ -1,5 +1,7 @@
 package problem.predator;
 
+
+
 /**
  * Created by Dev on 26/04/2017.
  */
@@ -13,12 +15,20 @@ public class FeatureExtractor {
      * @return a representation of current state, as seen by thisPredator
      */
     public static double[] getStateRepresentation(Animal thisPredator, Animal otherPredator, Animal prey) {
-        double[] state = null;
 
-        // *** YOUR CODE HERE **********************************************************************
 
+        // *** YOUR CODE HERE *********************************************************************
+        double disFromotherPrey =   Math.sqrt(Math.pow( thisPredator.x - prey.x,2) +  Math.pow(thisPredator.y - prey.y,2));
+        double disFromotherPredator =   Math.sqrt(Math.pow( thisPredator.x - otherPredator.x,2) +  Math.pow(thisPredator.y - otherPredator.y,2));
+        double guu =   Math.sqrt(Math.pow( otherPredator.x - prey.x,2) +  Math.pow(otherPredator.y - prey.y,2));
+        double ppp =   Math.sqrt(Math.pow( thisPredator.x - 0,2) +  Math.pow(thisPredator.y - 0,2));
+        double kkk =   Math.sqrt(Math.pow( thisPredator.x - 20,2) +  Math.pow(thisPredator.y - 20,2));
         // *** END OF YOUR CODE ********************************************************************
-
+        //double x = thisPredator.x - otherPredator.x;
+        //double y = thisPredator.y - otherPredator.y;
+        //state[2] = thisPredator.x - prey.x;
+        //state[3] = thisPredator.y - prey.y;
+        double[] state = {disFromotherPrey,disFromotherPredator,guu,ppp,kkk};
         return state;
     }
 
