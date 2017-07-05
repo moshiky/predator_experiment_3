a5b9907c82851581e5bda4c68223683332de53aa
moshiky <moshec40@gmail.com>
2017-07-05 17:24:00 +0300

    * code completed

--- src/main/java/problem/predator/SimilarityManager.java
+++ src/main/java/problem/predator/SimilarityManager.java
@@ -27,6 +27,48 @@ public class SimilarityManager {
 
         // *** YOUR CODE HERE **********************************************************************
 
+        double[] stt = state.clone();
+
+        if (action == 0)//up x-1
+        {
+            stt[0] -= 1;
+            stt[2] -= 1;
+        }
+        else if (action == 1)//down x+1
+        {
+            stt[0] += 1;
+            stt[2] += 1;
+        }
+        else if (action == 2)//left y-1
+        {
+            stt[1] -= 1;
+            stt[3] -= 1;
+        }
+        else if (action == 3)//right y+1
+        {
+            stt[1] += 1;
+            stt[3] += 1;
+        }
+
+        double tmp;
+
+        double[] stt1 = stt.clone();
+        tmp = stt1[3];
+        stt1[3] = stt1[2];
+        stt1[2] = tmp;
+        similarityRecords.add(new SimilarityRecord(stt1, 1, 1));
+
+
+        double[] stt2 = stt.clone();
+        tmp = stt2[3];
+        stt2[3] = stt2[2];
+        stt2[2] = tmp;
+        tmp  = stt2[0];
+        stt2[0] = stt[1];
+        stt[1] = tmp;
+        similarityRecords.add(new SimilarityRecord(stt2, 3, 0.6));
+
+
         // *** END OF YOUR CODE ********************************************************************
 
         return similarityRecords;
